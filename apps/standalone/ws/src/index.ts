cd454d573480c0b38e427ff3f5ff4dc5c4efc3b2
Jaga <jagadheesh6@gmail.com>
2025-05-04 14:12:16 +0530

    release - ws 1.0.0-beta.6

--- apps/standalone/ws/src/index.ts
+++ apps/standalone/ws/src/index.ts
@@ -1,6 +1,6 @@
 import { WsClient } from '@nescord/ws';
 import { EventType } from '@nescord/ws/lib/enum/event-type.enum';
-import { IntentsBitField } from 'discord.js';
+import { GatewayIntentBits } from 'discord.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,12 +9,13 @@ const token = process.env.DISCORD_BOT_TOKEN as string;
 const gRPCHost = process.env.GRPC_HOST as string;
 const intents = process.env.DISCORD_INTENTS as string;
 const events = process.env.EVENTS as string;
+const allEvents = Object.values(EventType);
 
 new WsClient({
   token,
   gRPCHost,
   intents: (intents?.split(',') || []).map(
-    (intent) => intent as unknown as IntentsBitField,
+    (intent) => intent as unknown as GatewayIntentBits,
   ),
-  events: events === '*' ? events : (events?.split(',') as EventType[]),
+  events: events === '*' ? allEvents : (events?.split(',') as EventType[]),
 });
